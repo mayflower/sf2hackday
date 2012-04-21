18316325469891d7044d4f88c5ff1ee2024788ff
Lars Strojny <lars@strojny.net>
2012-04-21 11:18:28 +0200

    Removing command experiment. Now lives in experiments/services-as-commands

--- src/LogTweets/Bundle/OrganisationBundle/Command/CommandInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace LogTweets\Bundle\OrganisationBundle\Command;
-
-interface CommandInterface
-{
-    public function execute(ParametersInterface $parameters);
-
-    public function rollback(ParametersInterface $parameters);
-}
